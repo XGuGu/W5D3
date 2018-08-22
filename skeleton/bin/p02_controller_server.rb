2f86d299a44b919379242c4e6f5e6399169731bb
App Academy Student <appacademy@Apps-Mac-mini-3.local>
2018-08-22 17:34:07 -0400

    end of w5d3

--- skeleton/bin/p02_controller_server.rb
+++ skeleton/bin/p02_controller_server.rb
@@ -10,6 +10,7 @@ class MyController < ControllerBase
     end
   end
 end
+
 app = Proc.new do |env|
   req = Rack::Request.new(env)
   res = Rack::Response.new
@@ -17,8 +18,9 @@ app = Proc.new do |env|
   res.finish
 end
 
+
+
 Rack::Server.start(
   app: app,
   Port: 3000
 )
-
--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -8,33 +8,57 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
+    @params = req.params.merge(route_params)
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "Double render error!" if already_built_response?
+    @res.status = 302
+    @res["Location"] = url
+    @already_built_response = true
+    session.store_session(@res)
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "Double render error!" if already_built_response?
+    @res.write(content)
+    @res.set_header('Content-Type', content_type)
+    @already_built_response = true
+    session.store_session(@res)
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    path = File.join("views",self.class.name.underscore, "#{template_name}.html.erb")
+    file = File.read(path)
+    self.render_content(ERB.new(file).result(binding), "text/html")
   end
+
+
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
+
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    self.render(name) unless already_built_response?
+
   end
 end
-
